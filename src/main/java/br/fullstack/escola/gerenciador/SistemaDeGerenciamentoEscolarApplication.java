e3b7b934bf3358be3809d36b68168736a6118996
Leonel Goulart <105310217+Leogolass@users.noreply.github.com>
2024-03-03 18:15:14 -0300

    Testing Commit

---
+++ src/main/java/br/fullstack/escola/gerenciador/SistemaDeGerenciamentoEscolarApplication.java
@@ -0,0 +1,13 @@
+package br.fullstack.escola.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGerenciamentoEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGerenciamentoEscolarApplication.class, args);
+	}
+
+}
